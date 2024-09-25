b3071a6c1d4f49e3004a316f8ec7adb37e287e2f
unknown <reyhanehgithub@gmail.com>
2024-09-25 15:50:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        firstname:"reza",
+        lastanme:"razavi",
+        age:20
+    },
+    {
+        firstname:"ali",
+        lastanme:"akbari",
+        age:14
+    },
+    {
+        firstname:"akbar",
+        lastanme:"mohseni",
+        age:50
+    },
+    {
+        firstname:"hamed",
+        lastanme:"mohammadi",
+        age:40
+    },
+    {
+        firstname:"majid",
+        lastanme:"asadi",
+        age:35
+    }
+]
+
